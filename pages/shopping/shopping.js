2fe8e43ff98630ed31dd32928a1f38be48182a32
machao <15777100748@163.com>
2021-03-23 23:34:11 +0800

    购物,商品详情

--- pages/shopping/shopping.js
+++ pages/shopping/shopping.js
@@ -12,14 +12,38 @@ create.Page(store, {
 
   data: {
     userInfo: {},
+    typeConfig: [
+      {value: 1, label: '营业区'}, { value: 2, label: '配电室' }, { value: 3, label: '油罐区' },
+      {value: 4, label: '办公室'}, { value: 5, label: '活动室' }, { value: 6, label: '厨房用品' },
+      {value: 7, label: '卫浴用品'}, { value: 8, label: '其他' }
+    ],
+    activeTypeIndex: 0,
+    goods: {}
   },
 
   onLoad(options) {
-
+    this.getGoodsList(this.data.typeConfig[0].value);
   },
 
   onUnload() {
 
+  },
+
+  handleChangeType(e) {
+    const index = e.detail;
+    this.setData({activeTypeIndex: index});
+    this.getGoodsList(this.data.typeConfig[index].value)
+  },
+
+  getGoodsList(typeValue) {
+    const list = [1,2,3,4,5,6,7,8,11,22,33,44,55,66].map(item => ({url: 'https://img.xiaopiu.com/userImages/img525317678d80e10.jpg'}))
+    this.setData({
+      ['goods.' + typeValue]: list
+    })
+  },
+
+  toDetail(e) {
+    wxUtils.backOrNavigate(`/pages/goodsDetail/goodsDetail?id=${Math.random()}`)
   }
 
 });
